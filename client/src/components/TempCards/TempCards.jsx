a2ca9a8de0739dca95318696cb2cb05a7bf7a6b8
sebgutierrez <84500161+sebgutierrez@users.noreply.github.com>
2025-05-26 05:20:43 -0500

    removed scroll buttons

--- client/src/components/TempCards/TempCards.jsx
+++ client/src/components/TempCards/TempCards.jsx
@@ -1,69 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 
-import leftScroll from "../../assets/leftScrollSVG.svg";
-import rightScroll from "../../assets/rightScrollSVG.svg";
-
 const TempCards = (props) => {
-  const [cardsComputedWidth, setCardsComputedWidth] = useState(0);
-  const cardsRef = useRef(null);
-  const leftScrollRef = useRef(null);
-  const rightScrollRef = useRef(null);
-  const xPosition = useRef(0);
-
-  const gapSizePx = 8;
-  const cardSizePx = 120;
-  const stride = 2;
-  const cardsActualWidth = gapSizePx * 6 + cardSizePx * 7;
-
-  function onRightScroll() {
-    // On right scroll, a negative offset is the same as shifting the element to the right
-
-    // check if computed width is less than actual width. larger screens don't have defined card widths
-    if (cardsComputedWidth < cardsActualWidth) {
-      // newOffset: how much to shift the cards by
-      let newOffset = (cardSizePx + gapSizePx) * stride;
-      // Bound checking:
-      if (
-        xPosition.current - newOffset >
-        cardsActualWidth * -1 + cardsComputedWidth
-      ) {
-        xPosition.current -= newOffset;
-      } else {
-        xPosition.current = cardsActualWidth * -1 + cardsComputedWidth;
-      }
-      cardsRef.current.style.transform = `translateX(${xPosition.current}px)`;
-    }
-  }
-
-  function onLeftScroll() {
-    // On left scroll, a positive offset is the same as shifting the element to the left
-    if (cardsComputedWidth < cardsActualWidth) {
-      let newOffset = (cardSizePx + gapSizePx) * stride;
-      // Bound checking. Only difference is ...
-      if (xPosition.current + newOffset < cardsComputedWidth * -1) {
-        xPosition.current += newOffset;
-      } else {
-        xPosition.current = 0;
-      }
-      cardsRef.current.style.transform = `translateX(${xPosition.current}px)`;
-    }
-  }
-
-  function handleResize() {
-    setCardsComputedWidth(cardsRef.current.offsetWidth);
-  }
-
-  useEffect(() => {
-    if (cardsRef.current) {
-      setCardsComputedWidth(cardsRef.current.offsetWidth);
-      window.addEventListener("resize", handleResize);
-    }
-    return () => {
-      if (cardsRef.current) {
-        window.removeEventListener("resize", handleResize);
-      }
-    };
-  }, [cardsRef]);
 
   let listItems = [];
 
@@ -107,30 +44,11 @@ const TempCards = (props) => {
 
   return (
     <div className="flex w-full">
-      <img
-        src={leftScroll}
-        className="lg:hidden w-[25px] h-[60px] absolute -left-1 top-[6.4rem] sm:top-[4.9rem]"
-        style={{ zIndex: 5 }}
-        ref={leftScrollRef}
-        onClick={onLeftScroll}
-      ></img>
       <ol
         className="grid grid-flow-col justify-stretch gap-x-2 w-full"
-        ref={cardsRef}
-        style={{
-          transitionDuration: "0.25s",
-          transform: `translateX(${xPosition.current}px)`,
-        }}
       >
         {listItems}
       </ol>
-      <img
-        src={rightScroll}
-        className="lg:hidden w-[25px] h-[60px] absolute -right-1 top-[6.4rem] sm:top-[4.9rem]"
-        style={{ zIndex: 5 }}
-        ref={rightScrollRef}
-        onClick={onRightScroll}
-      ></img>
     </div>
   );
 };
