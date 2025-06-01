c2a5b8a8c6ec91da48779776700982f1aa4f1430
sebgutierrez <84500161+sebgutierrez@users.noreply.github.com>
2025-06-01 01:27:33 -0500

    integrated maplibre

--- client/src/components/ForecastSection/ForecastSection.jsx
+++ client/src/components/ForecastSection/ForecastSection.jsx
@@ -1,24 +1,31 @@
-import React, { useState } from "react";
-
+import React from 'react';
 import ForecastChart from "../ForecastChart/ForecastChart";
-import ForecastList from "../ForecastList/ForecastList";
 
 const ForecastSection = (props) => {
-  const [view, setView] = useState("chart");
-
+  
   return (
-    <div className="flex flex-col w-full my-2">
+    <div className="flex flex-col w-full mb-2 mt-2 overflow-y-auto">
       <div className="flex w-full">
         <div className="flex flex-col sm:flex-row sm:items-end justify-start sm:justify-between sm:align-bottom w-full">
-          <h2 className="text-slate-600 font-bold text-left text-lg lg:text-2xl">
-            24-Hour Forecast
-          </h2>
-          <span className="text-slate-500 text-left text-md">
-            {props.modelInfo.region}
+          <div className="flex items-end">
+            <h2 className="text-slate-600 font-bold text-left text-md md:text-lg lg:text-2xl">
+              Hourly Forecast
+            </h2>
+            <span className="text-slate-500 pb-0.5 pl-2">5/25/2025</span>
+          </div>
+          <span className="text-slate-500 text-left text-md pb-0.5">
+            {props.regionName}
           </span>
         </div>
       </div>
       <div className="my-2 px-0 h-px bg-slate-900/10"></div>
+      <div className="w-full overflow-x-scroll lg:overflow-auto">
+        <ForecastChart
+          isCelsius={props.isCelsius}
+          apiData={props.apiData}
+          regionName={props.regionName}
+        ></ForecastChart>
+      </div>
     </div>
   );
 };
--- client/src/pages/Forecast/Forecast.jsx
+++ client/src/pages/Forecast/Forecast.jsx
@@ -1,9 +1,7 @@
-"use client";
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
 
 import '../../App.css';
 import '../Forecast/Forecast.css';
-import questionMark from "../../assets/question-mark.svg"
 
 import Header from '../../components/Header/Header';
 import ForecastForm from '../../components/ForecastForm/ForecastForm';
@@ -17,7 +15,7 @@ function Forecast() {
     model: "",
     geo: {
       lat: 40,
-      long: -60
+      long: -80
     },
     predictions: {} 
   });
@@ -30,35 +28,64 @@ function Forecast() {
   
   // Temporary static temperature data
 
-  const openWeatherData = {
-		c: [[new Date('2025-05-24T00:00:00').getTime(), -17.3], [new Date('2025-05-24T03:00:00').getTime(), -17.8], [new Date('2025-05-24T06:00:00').getTime(), -19.7], [new Date('2025-05-24T09:00:00').getTime(), -15.4], [new Date('2025-05-24T12:00:00').getTime(), -13.8], [new Date('2025-05-24T15:00:00').getTime(), -11.7], [new Date('2025-05-24T18:00:00').getTime(), -15.2],  [new Date('2025-05-24T21:00:00').getTime(), -14.3], [new Date('2025-05-25T00:00:00').getTime(), -11.0]],
-		f: [[new Date('2025-05-24T00:00:00').getTime(), 0.9], [new Date('2025-05-24T03:00:00').getTime(), -0.1], [new Date('2025-05-24T06:00:00').getTime(), -3.5], [new Date('2025-05-24T09:00:00').getTime(), 4.3], [new Date('2025-05-24T12:00:00').getTime(), 7.2], [new Date('2025-05-24T15:00:00').getTime(), 10.9], [new Date('2025-05-24T18:00:00').getTime(), 4.7],  [new Date('2025-05-24T21:00:00').getTime(), 6.2], [new Date('2025-05-25T00:00:00').getTime(), 12.2]]
-	}
-	const modelData = {
-		c: [[new Date('2025-05-24T00:00:00').getTime(), null], [new Date('2025-05-24T03:00:00').getTime(), null], [new Date('2025-05-24T06:00:00').getTime(), null], [new Date('2025-05-24T09:00:00').getTime(), null], [new Date('2025-05-24T12:00:00').getTime(), null], [new Date('2025-05-24T15:00:00').getTime(), null], [new Date('2025-05-24T18:00:00').getTime(), -15.2], [new Date('2025-05-24T21:00:00').getTime(), -15.6], [new Date('2025-05-25T00:00:00').getTime(), -16.3]],
-		f: [[new Date('2025-05-24T00:00:00').getTime(), null], [new Date('2025-05-24T03:00:00').getTime(), null], [new Date('2025-05-24T06:00:00').getTime(), null], [new Date('2025-05-24T09:00:00').getTime(), null], [new Date('2025-05-24T12:00:00').getTime(), null], [new Date('2025-05-24T15:00:00').getTime(), null], [new Date('2025-05-24T18:00:00').getTime(), 4.7], [new Date('2025-05-24T21:00:00').getTime(), 3.9], [new Date('2025-05-25T00:00:00').getTime(), 2.6]]
+  let apiData = {
+    "Houston, TX, USA": {
+      c: new Map([
+        [new Date('2025-05-24T00:00:00').getTime(), -17.3], 
+        [new Date('2025-05-24T03:00:00').getTime(), -17.8], 
+        [new Date('2025-05-24T06:00:00').getTime(), -19.7], 
+        [new Date('2025-05-24T09:00:00').getTime(), -15.4], 
+        [new Date('2025-05-24T12:00:00').getTime(), -13.8], 
+        [new Date('2025-05-24T15:00:00').getTime(), -11.7], 
+        [new Date('2025-05-24T18:00:00').getTime(), -15.2], 
+        [new Date('2025-05-24T21:00:00').getTime(), -14.3]
+      ]),
+      f: new Map([
+        [new Date('2025-05-24T00:00:00').getTime(), 0.9], 
+        [new Date('2025-05-24T03:00:00').getTime(), -0.1], 
+        [new Date('2025-05-24T06:00:00').getTime(), -3.5], 
+        [new Date('2025-05-24T09:00:00').getTime(), 4.3], 
+        [new Date('2025-05-24T12:00:00').getTime(), 7.2], 
+        [new Date('2025-05-24T15:00:00').getTime(), 10.9], 
+        [new Date('2025-05-24T18:00:00').getTime(), 4.7],  
+        [new Date('2025-05-24T21:00:00').getTime(), 6.2]
+      ])
+    }
 	}
 
+  let modelData = {
+    Houston: {
+      LSTM: {
+        c: new Map(),
+        f: new Map()
+      }
+    }
+  }
+
   return (
-    <div className='flex flex-col relative bg-white'> 
+    <div className='relative min-h-[100vh] flex flex-col bg-white'> 
         <Header></Header>
-        <div className="flex flex-col md:flex-row items-center md:items-start pb-2 md:pb-0 pt-2 md:pt-0 relative px-4 md:my-4">
+        <div className="absolute bg-[#2c74ff] w-full h-[50vh]"></div>
+        <div className="relative flex flex-col items-center min-h-[calc(100vh-80px)] pt-[calc(50vh-200px)] px-4">
           <ForecastForm modelInfo={modelInfo} setModelInfo={setModelInfo}></ForecastForm>
-          <div className="relative w-full flex flex-col max-h-[calc(100vh-72px)] px-0 md:px-2 overflow-y-auto">
-            <LibreMap modelInfo={modelInfo}></LibreMap>
-            <div className="flex flex-col items-start py-3">
-              <h2 className="text-slate-700 text-xl">Settings</h2>
-              <label htmlFor="degree" className="text-slate-700 text-md py-1">Temperature Unit</label>
-              <select name="degree" id="degree" className="bg-white w-[142px] py-1 text-slate-500 hover:bg-gray-50 border-2 border-slate-200" onChange={flipIsCelsius}>
-                <option value="°F">Fahrenheit °F</option>
-                <option value="°C">Celsius °C</option>
-              </select>
-            </div>
-            {
-              modelInfo.region !== '' && modelInfo.model !== '' ? 
-                                                                (<SectionContainer openWeatherData={openWeatherData} modelData={modelData} modelInfo={modelInfo} setModelInfo={setModelInfo} isCelsius={isCelsius}></SectionContainer>)
-                                                                :
-                                                                (null)
+          <div className="relative w-full flex flex-col px-0 md:px-20 pb-6">
+            { 
+              modelInfo.region !== '' && modelInfo.model !== '' &&
+              (
+                <div>
+                  <div className="w-full flex flex-col items-start py-3">
+                    <h2 className="text-slate-700 text-xl">Settings</h2>
+                    <div className="w-full my-2 px-0 h-px bg-slate-900/5"></div>
+                    <label htmlFor="degree" className="text-slate-700 text-md py-1">Temperature Unit</label>
+                    <select name="degree" id="degree" className="bg-white w-[142px] py-1 text-slate-500 hover:bg-gray-50 border-2 border-slate-200" onChange={flipIsCelsius}>
+                      <option value="°F">Fahrenheit °F</option>
+                      <option value="°C">Celsius °C</option>
+                    </select>
+                  </div>
+                  <LibreMap modelInfo={modelInfo} apiData={apiData} isCelsius={isCelsius}></LibreMap>
+                  <SectionContainer apiData={apiData} modelData={modelData} modelInfo={modelInfo} setModelInfo={setModelInfo} isCelsius={isCelsius}></SectionContainer>
+                </div>
+              )
             }
           </div>  
         </div>
