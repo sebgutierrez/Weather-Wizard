09ffe24da42334a7f5af2dfad50d5aef31c8ffc6
sebgutierrez <84500161+sebgutierrez@users.noreply.github.com>
2025-06-09 21:49:29 -0500

    renamed and modified svgs/pngs

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,7 @@ import {defineConfig, loadEnv} from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
-  const {VITE_GOOGLE_MAPS_API_KEY = ''} = loadEnv(mode, process.cwd(), '');
   return {
-    define: {
-      'process.env.VITE_GOOGLE_MAPS_API_KEY': JSON.stringify(VITE_GOOGLE_MAPS_API_KEY)
-    }
   };
 });
 
